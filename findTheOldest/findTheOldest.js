6e9429f446c4fd2daa3c1a4086724f2a72086ca0
William Goins <de777urest@gmail.com>
2020-12-20 12:45:20 -0700

    Complete findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(peopleArr) {
+    let oldest;
+    let maxAge = -1;
+    peopleArr.forEach(person => {
+        if("yearOfDeath" in person) {
+            if(person.yearOfDeath - person.yearOfBirth > maxAge) {
+                maxAge = person.yearOfDeath - person.yearOfBirth;
+                oldest = person;
+            }
+        }
+        else {
+            if(2020 - person.yearOfBirth > maxAge) {
+                maxAge = 2020 - person.yearOfBirth;
+                oldest = person;
+            }
+        }
+    });
+    return oldest;
 }
 
 module.exports = findTheOldest
