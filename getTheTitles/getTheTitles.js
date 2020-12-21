293e9659dc63967da04e94685ded0f523a387952
William Goins <de777urest@gmail.com>
2020-12-20 18:27:45 -0700

    Completed getTheTitles and leapYears

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArr) {
+    return flattenArray(bookArr);
+}
 
+function flattenArray(data) {
+    // our initial value this time is a blank array
+    const initialValue = [];
+
+    // call reduce on our data
+    return data.reduce((total, value) => {
+        // if the value is an array then recursively call reduce
+        // if the value is not an array then just concat our value
+        return total.concat(value.title);
+    }, initialValue);
 }
 
 module.exports = getTheTitles;
--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(aYear) {
+    isLeapYear = false;
 
+    if (aYear % 100 === 0) {
+        if (aYear % 400 === 0) {
+            isLeapYear = true;
+        }
+    }
+    else if (aYear % 4 === 0 ) {
+        isLeapYear = true;
+    }
+
+    return isLeapYear;
 }
 
 module.exports = leapYears
