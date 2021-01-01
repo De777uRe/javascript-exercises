8ec3e000ae18ce6c7d1368db8522b6f94db88755
William Goins <de777urest@gmail.com>
2021-01-01 11:15:37 -0700

    Completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let originalArray = args[0];
 
+    for(let i = 1; i < args.length; i++) {
+        if(originalArray.includes(args[i])) {
+            if(originalArray.length === 1) {
+                originalArray.pop();
+            }
+            else {
+                let index = originalArray.indexOf(args[i]);
+                originalArray.splice(index, 1);
+            }
+        }
+    }
+
+    return originalArray;
 }
 
 module.exports = removeFromArray
