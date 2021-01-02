ccb34ea3320ef4a701d6420539146f293a93387c
William Goins <de777urest@gmail.com>
2021-01-02 15:37:03 -0700

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFarenheit) {
+    let tempCelsius = (tempFarenheit - 32) * (5/9);
+    return parseFloat(tempCelsius.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+    let tempFarenheit = (tempCelsius * (9/5)) + 32;
+    return parseFloat(tempFarenheit.toFixed(1));
 }
 
 module.exports = {
